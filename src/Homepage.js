ad77099693a024fe5be11afb72ea5c02e1ff6149
Johns, Erik Michael <erikj3520@gmail.com>
2024-04-01 12:14:17 -0600

    Added Newsletter

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,8 +1,18 @@
 import Carousel from "./Components/Carousel";
 import Navbar from "./Components/Navbar";
 import FeaturedPosts from "./Components/FeaturedPosts";
+import Footer from "./Components/Footer";
+import {FaArrowRight} from "react-icons/fa6";
 
 const Homepage = () => {
+
+    /*
+    document.querySelector('.newsletter-form-submit').addEventListener('mousedown', (e) => {
+        e.preventDefault();
+        document.querySelector('form').classList.add('done');
+    })
+     */
+
     return (
         <div className="homepage">
             <Navbar />
@@ -15,6 +25,21 @@ const Homepage = () => {
                 </div>
                 <FeaturedPosts />
             </div>
+
+            <div className="newsletter-form">
+                <h1>
+                    W<span>a</span>n<span>t to</span> S<span>t</span>a<span>y in</span> t<span>h</span>e <span>K</span>now?
+                </h1>
+                <p>Subscribe to my newsletter below to get notified about any new posts I make.</p>
+                <form>
+                    <input type="email" id="newsletter" className="newsletter-form-input" placeholder="Enter your email" />
+                    <button type="submit" className="newsletter-form-submit">
+                        <span className="before-submit"><FaArrowRight color={"white"}/></span>
+                        <span className="after-submit">Thanks for Subscribing!</span>
+                    </button>
+                </form>
+            </div>
+            <Footer />
         </div>
     )
 };
