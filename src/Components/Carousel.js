e1eef094d914fbc8acea82f3715f5d50a5e4b35f
Johns, Erik Michael <erikj3520@gmail.com>
2024-03-31 23:06:33 -0600

    Implemented basic carousel

---
+++ src/Components/Carousel.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+const Carousel = () => {
+
+    const carouselContent = [
+        {
+            img: "https://images.unsplash.com/photo-1632114448815-e03ae5b7dcc0?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            imgAlt: "Amalfi Coast",
+            mainText: "The Amalfi Coast",
+            subText: "The ins and outs of the Amalfi Coast: where to stay, where to eat, how to enjoy! Learn about where I stayed, what I did, and what I think you should do.",
+        },
+        {
+            img: "https://images.unsplash.com/photo-1590089349708-9842d3f6ca25?q=80&w=3948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            imgAlt: "Cliffs of Moher",
+            mainText: "Ireland",
+            subText: "The ins and outs of the Amalfi Coast: where to stay, where to eat, how to enjoy! Learn about where I stayed, what I did, and what I think you should do.",
+        },
+        {
+            img: "https://images.unsplash.com/photo-1585414298822-bcfc1e276cc8?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            imgAlt: "South of France",
+            mainText: "The French Riviera",
+            subText: "The ins and outs of the Amalfi Coast: where to stay, where to eat, how to enjoy! Learn about where I stayed, what I did, and what I think you should do.",
+        }
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % carouselContent.length);
+        }, 5000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return (
+        <div className="hero-container">
+            <img src={carouselContent[currentIndex].img} alt={carouselContent[currentIndex].imgAlt} />
+            <div className="hero-text-container">
+                <p>Latest Post</p>
+                <h1>{carouselContent[currentIndex].mainText}</h1>
+                <h4>{carouselContent[currentIndex].subText}</h4>
+                <button>Read More</button>
+            </div>
+        </div>
+    )
+}
+
+export default Carousel;
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,3 +1,4 @@
+import Carousel from "./Components/Carousel";
 import Navbar from "./Components/Navbar";
 
 const Homepage = () => {
@@ -5,20 +6,11 @@ const Homepage = () => {
         <div class="homepage">
             <Navbar />
             <div class="hero-container">
-                <img src="https://images.unsplash.com/photo-1632114448815-e03ae5b7dcc0?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="hero"/>
-                <div class="hero-text-container">
-                    <p>Latest Post</p>
-                    <h1>T<span>he</span> <span>A</span>mal<span>f</span>i Coas<span>t</span></h1>
-                    <h4>
-                        The ins and outs of the Amalfi Coast: where to stay, where to eat, how to enjoy!
-                        Learn about where I stayed, what I did, and what I think you should do.
-                    </h4>
-                    <button>Read Now</button>
-                </div>
-                <div class="hero-pagination">
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                <Carousel />
+                <div class="homepage-section">
+                    <div class="section-1">
+                        <h1>My name is <em>Erik</em></h1>
+                    </div>
                 </div>
             </div>
         </div>
