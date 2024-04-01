c916d634b3a998385e6af10469ec818b619c9d9f
Johns, Erik Michael <erikj3520@gmail.com>
2024-03-31 21:10:56 -0600

    Initial Push

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <div className="navbar-container">
+            <h1><span>Tr</span>a<span>v</span>el Wit<span>h</span> <span>E</span>rik</h1>
+            <ul>
+                <li>Posts</li>
+                <li>Destinations</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import './fonts/HV-Cocktail-Alternates.woff2';
+import './fonts/HV-Cocktail-Regular.woff2';
+import './fonts/NordicSalt-Regular.woff';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
